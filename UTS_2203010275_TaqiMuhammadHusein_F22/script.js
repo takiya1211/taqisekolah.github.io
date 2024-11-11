1c8001b07ec0524325fa2448d35f8a585151698e
takiya1211 <154291994+takiya1211@users.noreply.github.com>
2024-11-11 18:41:31 +0700

    Add files via upload

---
+++ UTS_2203010275_TaqiMuhammadHusein_F22/script.js
@@ -0,0 +1,4 @@
+document.getElementById('menuToggle').addEventListener('click', function() {
+    const menu = document.querySelector('.taqi_menu');
+    menu.classList.toggle('show'); // Toggles the visibility of the menu
+});
